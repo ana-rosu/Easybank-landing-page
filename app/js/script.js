34dec97b6aaacbbb55c95f3a38ecc812aca44189
Ana Roșu <anarosu766@gmail.com>
2022-08-13 17:30:39 +0300

    finished styles

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,6 @@
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
-const headerMenu = document.querySelector(".header__menu");
+const headerLinks = document.querySelector(".header__links");
 
 header.addEventListener("click", function () {
   if (header.classList.contains("open")) {
@@ -10,8 +10,9 @@ header.addEventListener("click", function () {
     overlay.classList.remove("fade-in");
     overlay.classList.add("fade-out");
 
-    headerMenu.classList.remove("fade-in");
-    headerMenu.classList.add("fade-out");
+    headerLinks.classList.add("hide-for-mobile");
+    headerLinks.classList.remove("fade-in");
+    headerLinks.classList.add("fade-out");
   } else {
     //open menu
     header.classList.add("open");
@@ -19,7 +20,8 @@ header.addEventListener("click", function () {
     overlay.classList.remove("fade-out");
     overlay.classList.add("fade-in");
 
-    headerMenu.classList.remove("fade-out");
-    headerMenu.classList.add("fade-in");
+    headerLinks.classList.remove("hide-for-mobile");
+    headerLinks.classList.remove("fade-out");
+    headerLinks.classList.add("fade-in");
   }
 });
